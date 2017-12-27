52d98a193088a2ce949279a67d0acb9cb824763b
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-27 17:23:42 +0100

    basiscs
    Ill make this thing work later

---
+++ main.py
@@ -0,0 +1,19 @@
+word = words.csv #seznamslov
+dashnumber = len(word)
+print("_ "*len(word))
+letters = #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
+for letters in word
+input("what letter are you guessing?": )
+    if len(a) == 0
+        print("Yout didnt write anything")
+    elif len(a) >= 1
+        print("write just one letter, not a snkscks")
+if input = letters[] #jednomu prvku ze seznamu
+    print letters[] in word
+else print("wrong number",)
+    print(hangman + 1) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+#
+
+
+
+
