902a614146092944f17c904284d685b7c23f7921
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-30 13:25:59 +0100

    I have no idea how input works, but this is how the product shoudl look like

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ word = words.csv #seznamslov
 hidden_word = "_ "*len(word)
 print(hidden_word)
 letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
+while x = yes
+    for letters 
+        input("what letter are you guessing?":)
+        if len(input) == 0
+            print("Yout didnt write anything")
+        elif len(a) >= 1
+         print("write just one letter, not a snkscks")
 
-for letters:
-    input("what letter are you guessing?":)
-    if len(input) == 0
-        print("Yout didnt write anything")
-    elif len(a) >= 1
-        print("write just one letter, not a snkscks")
+    if input = letters #jednomu prvku ze seznamu
+        print(hangman)
+        hidden_word[letters[input]] == input
+      print hidden_word
 
-if input = letters #jednomu prvku ze seznamu
-    print(hangman)
-
-
-else print("wrong letter",)
-    hangman = hangman + 1
-    print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
-    print(letters)
+    else print("wrong letter",)
+       hangman = hangman + 1
+       print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+       print(hidden_word)
+    input("do you wish to plaz again?": )
+        if input != no ; != yes
+            print("youre a dumbass")
 
 
 
