63f33da97dc626fccb1ef9f3d30d80496763500b
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-30 15:57:04 +0100

    found out how to use inputs, not sure if correctletter works though

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ word = words.csv #seznamslov
 hidden_word = "_ "*len(word)
 print(hidden_word)
 letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
-while x = yes
-    for letters 
-        input("what letter are you guessing?":)
-        if len(input) == 0
-            print("Yout didnt write anything")
-        elif len(a) >= 1
-         print("write just one letter, not a snkscks")
 
-    if input = letters #jednomu prvku ze seznamu
-        print(hangman)
-        hidden_word[letters[input]] == input
-      print hidden_word
+input_word = input("what letter are you guessing?:")
+if len(input_word) == 0:
+    print("Yout didnt write anything")
+elif len(input_word) >= 1:
+    print("write just one letter, not a whole essay")
 
-    else print("wrong letter",)
-       hangman = hangman + 1
-       print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
-       print(hidden_word)
-    input("do you wish to plaz again?": )
-        if input != no ; != yes
-            print("youre a dumbass")
+if input_word =    #jednomu prvku ze seznamu
+    print(hangman)
+    hidden_word[letters[input_word]] == input_word
+    print hidden_word
+
+else print("wrong letter",)
+hangman = hangman + 1
+print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+print(hidden_word)
+    
+input("do you wish to plaz again?": )  #input pro x
+    if input != no ; != yes
+        print("youre a dumbass")
 
 
 
