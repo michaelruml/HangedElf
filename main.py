28a4fdd114fb3b850829579d42871c8dc9cac381
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-30 12:58:54 +0100

    made it more redable, started figuring out how to make letters appaear

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 word = words.csv #seznamslov
-dashnumber = len(word)
-print("_ "*len(word))
-letters = #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
-for letters in word
-input("what letter are you guessing?": )
-    if len(a) == 0
+hidden_word = "_ "*len(word)
+print(hidden_word)
+letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
+
+for letters:
+    input("what letter are you guessing?":)
+    if len(input) == 0
         print("Yout didnt write anything")
     elif len(a) >= 1
         print("write just one letter, not a snkscks")
-if input = letters[] #jednomu prvku ze seznamu
-    print letters[] in word
-else print("wrong number",)
-    print(hangman + 1) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
-#
 
+if input = letters #jednomu prvku ze seznamu
+    print(hangman)
+
+
+else print("wrong letter",)
+    hangman = hangman + 1
+    print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+    print(letters)
 
 
 
