715e2e1c519ef5239f97712919c735bd179900a7
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-31 13:10:43 +0100

    I dsont even know

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ print(hidden_word) #_ _ _ _
 letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
 hangman = print("a") 
 word_string = str(word)
-while playing = yes 
+while playing == yes:
     while hidden_word != letters:
         input_word = input("what letter are you guessing?:")
         if len(input_word) == 0:
-            print("Yout didnt write anything")
+            print("You  didnt write anything")
         elif len(input_word) != 1:
             print("write just one letter, not a whole essay")
         elif len(input_word) == 1:
@@ -20,13 +20,6 @@ while playing = yes
                     if hidden_word == letters:
                         playing = input("do you wish to play again?(if you dont type yes, this thing is going to crash I think)")
             else:
-                print("wrong letter")
                 print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
                 print(hidden_word)
-    
-# vyřeším později   
-# on/off = input("do you wish to plaz again?": )  #input pro x g
-
-
-
-
+                print("wrong letter")
