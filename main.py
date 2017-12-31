89fff159f1d07f7bae8c9645a587d92fef51dc76
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-31 12:39:06 +0100

    its easier to read(I think) and it loops

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ print(hidden_word) #_ _ _ _
 letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
 hangman = print("a") 
 word_string = str(word)
-while hidden_word != letters:
-    input_word = input("what letter are you guessing?:")
-    if len(input_word) == 0:
-       print("Yout didnt write anything")
-    elif len(input_word) >= 1:
-        print("write just one letter, not a whole essay")
-    position = word.index(input_word) 
-    if input_word in letters:
-        print(hangman)
-        hidden_word[position] = input_word
-        print(hidden_word)
-    else:
-        print("wrong letter")
-        print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
-        print(hidden_word)
+while playing = yes 
+    while hidden_word != letters:
+        input_word = input("what letter are you guessing?:")
+        if len(input_word) == 0:
+            print("Yout didnt write anything")
+        elif len(input_word) != 1:
+            print("write just one letter, not a whole essay")
+        elif len(input_word) == 1:
+            if input_word in letters:
+                position = word.index(input_word)
+                hidden_word[position] = input_word
+                print(hangman)
+                print(hidden_word)
+                    if hidden_word == letters:
+                        playing = input("do you wish to play again?(if you dont type yes, this thing is going to crash I think)")
+            else:
+                print("wrong letter")
+                print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+                print(hidden_word)
     
 # vyřeším později   
 # on/off = input("do you wish to plaz again?": )  #input pro x g
