9512b3e3426296a6ef177e0d0607c53cef34a543
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-31 10:21:01 +0100

    další postup,no

--- main.py
+++ main.py
@@ -9,23 +9,19 @@ while hidden_word != letters:
        print("Yout didnt write anything")
     elif len(input_word) >= 1:
         print("write just one letter, not a whole essay")
-
+    letters.find(input word)
     if input_word in letters:
         print(hangman)
-        hidden_word[letters[input_word]] == input_word
+        hidden_word[] = input_word
         print(hidden_word)
-    
+    else:
         print("wrong letter")
-        hangman = hangman + 1
         print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
         print(hidden_word)
     
 # vyřeším později   
 # on/off = input("do you wish to plaz again?": )  #input pro x
-x = int(0)
-x = input("do you wish to play agiain(typE 1 OR 0):")
-if x != 0:
-    print("youre a dumbass")
+
 
 
 
