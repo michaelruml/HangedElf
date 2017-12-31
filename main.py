64323b890b68976558b5f8190b93680acfc3abd5
Martinneet <34580228+Martinneet@users.noreply.github.com>
2017-12-31 09:53:05 +0100

    wrong letter works, correct doesnt

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 word = "auto" #seznamslov #importnu to později
-hidden_word = "_ "*len(word)
+hidden_word = ["_ "*len(word)]
 print(hidden_word)
 letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
+hangman = print("a")
+while hidden_word != letters:
+    input_word = input("what letter are you guessing?:")
+    if len(input_word) == 0:
+       print("Yout didnt write anything")
+    elif len(input_word) >= 1:
+        print("write just one letter, not a whole essay")
 
-input_word = input("what letter are you guessing?:")
-if len(input_word) == 0:
-    print("Yout didnt write anything")
-elif len(input_word) >= 1:
-    print("write just one letter, not a whole essay")
-
-if input_word in letters:
-    print(hangman)
-    hidden_word[letters[input_word]] == input_word
-    print(hidden_word)
+    if input_word in letters:
+        print(hangman)
+        hidden_word[letters[input_word]] == input_word
+        print(hidden_word)
     
-    print("wrong letter")
-    hangman = hangman + 1
-    print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
-    print(hidden_word)
+        print("wrong letter")
+        hangman = hangman + 1
+        print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+        print(hidden_word)
     
 # vyřeším později   
 # on/off = input("do you wish to plaz again?": )  #input pro x
