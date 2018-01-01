f2266955a4a60e5f3384221635df5c1ca2d32fa4
Martinneet <34580228+Martinneet@users.noreply.github.com>
2018-01-01 13:11:52 +0100

    line 17 doesnt work properly, but otherwise its good

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ word = "auto" #slovo hledané #seznamslov #importnu to později
 hidden_word = ["_ "*len(word)]
 print(hidden_word) #_ _ _ _
 letters = list(word)  #seznam vytvořený z jendnotlivych písmen ve slove ##vyřeším to potom 
-hangman = print("a") 
-word_string = str(word)
-while playing == yes:
+word_string = str(word)   
+playing = "yes"
+while playing == "yes" :
     while hidden_word != letters:
         input_word = input("what letter are you guessing?:")
         if len(input_word) == 0:
@@ -15,11 +15,21 @@ while playing == yes:
             if input_word in letters:
                 position = word.index(input_word)
                 hidden_word[position] = input_word
-                print(hangman)
                 print(hidden_word)
-                    if hidden_word == letters:
-                        playing = input("do you wish to play again?(if you dont type yes, this thing is going to crash I think)")
             else:
-                print(hangman) #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
+                #každý hangman bude mít svoji hodnotu od 0 po 5 (nebo něco) 
                 print(hidden_word)
                 print("wrong letter")
+                lives = lives + 1
+                    if lives == 0:
+                        print(hangman_0)
+                    elif lives == 1:
+                        print(hangman_1)
+                    elif lives == 2
+                        print(hangan_2)
+                    elif lives == 3:
+                        print(hangman_3)
+                    elif lives == 4:
+                        print(hangman_4)
+                        print("you lost")
+                        playing = input("Play again?(yes or no)")     
